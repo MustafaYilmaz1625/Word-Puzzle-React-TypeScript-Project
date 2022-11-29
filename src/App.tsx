55ab2ddb8c26b349c5af24f54d164e920d2ff549
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-11-30 01:13:03 +0300

    add tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react"
 
 
 function App() {
+  // const [findWord, setFindWord]=useState(()=> {
+  //   return word[Math.floor(Math.random() * word.length)]
+  // })
+  const [enterLetters,setEnterLetters] = useState<string[]>([])
 
   return (
-    <div className="App">
-    hi helloooo
-    </div>
+   <div className="bg-red-500 min-h-screen">
+    Lose Winmb
+   </div>
   )
 }
 
