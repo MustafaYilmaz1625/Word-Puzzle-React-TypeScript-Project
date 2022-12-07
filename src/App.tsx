0fe21377861c398133b3728c75b4da332ad78846
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-12-08 00:17:42 +0300

    add keyboard.module.css on page.

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,25 @@ function App() {
   const [wordToGuess, setWordToGuess] = useState(getWord);
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
-  return <div style={{
-    maxWidth:"800px",
-    display: "flex",
-    flexDirection: "column",
-    gap:"2rem",
-    margin:"0 auto",
-    alignItems: "center"
-  }}>
-    <div style={{fontSize:"2rem", textAlign:"center"}}>Lose Win</div>
-    <HangmanDrawing/>
-    <HangmanWord/>
-    <Keyboard/>
-  </div>
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <div style={{alignSelf: "stretch"}}>
+        <Keyboard />
+      </div>
+    </div>
+  );
 }
 
 export default App;
