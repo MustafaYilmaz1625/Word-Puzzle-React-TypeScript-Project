45f8fe5c79b0c7a4ccda0ae2c183ceb6434eccef
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-12-07 23:20:48 +0300

    add HangmanDrawing.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import HangmanDrawing from "./HangmanDrawing";
+import HangmanWord from "./HangmanWord";
+import Keyboard from "./Keyboard";
+import words from "./wordList.json";
+
+const getWord = () => {
+  return words[Math.floor(Math.random() * words.length)];
+};
+
 function App() {
-  return <h1>hi</h1>;
+  const [wordToGuess, setWordToGuess] = useState(getWord);
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  return <div style={{
+    maxWidth:"800px",
+    display: "flex",
+    flexDirection: "column",
+    gap:"2rem",
+    margin:"0 auto",
+    alignItems: "center"
+  }}>
+    <div style={{fontSize:"2rem", textAlign:"center"}}>Lose Win</div>
+    <HangmanDrawing/>
+    <HangmanWord/>
+    <Keyboard/>
+  </div>
 }
 
 export default App;
